2ef896e2ef341c178497abfaec71ce2358d328d6
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-10-20 13:01:46 +0330

    //ُSet Budget Part

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ const expenditureValue = document.getElementById("expenditure-value");
 const balanceValue = document.getElementById("balance-amount");
 const list = document.getElementById("list");
 let tempAmount = 0;
+
+
+//ُSet Budget Part
+totalAmountButton.addEventListener("click",()=>{
+    tempAmount=totalAmount.value;
+    //empty or negative input
+    if(tempAmount === "" || tempAmount < 0){
+        errorMessage.classList.remove("hide");
+    }else{
+        errorMessage.classList.add("hide");
+        amount.innerHTML=tempAmount;
+    }
+    balanceValue.innerText=tempAmount - expenditureValue.innerText;
+    totalAmount.value="";
+})
