4ed144d6f536ee532db04e3b8e00481973fca251
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-10-20 20:15:27 +0330

    js next part

--- script.js
+++ script.js
@@ -12,19 +12,42 @@ const balanceValue = document.getElementById("balance-amount");
 const list = document.getElementById("list");
 let tempAmount = 0;
 
-
 //ُSet Budget Part
-totalAmountButton.addEventListener("click",()=>{
-    tempAmount=totalAmount.value;
-    //empty or negative input
-    if(tempAmount === "" || tempAmount < 0){
-        errorMessage.classList.remove("hide");
-    }else{
-        errorMessage.classList.add("hide");
-        amount.innerHTML=tempAmount;
-    }
-    balanceValue.innerText=tempAmount - expenditureValue.innerText;
-    totalAmount.value="";
-})
+totalAmountButton.addEventListener("click", () => {
+  tempAmount = totalAmount.value;
+  //empty or negative input
+  if (tempAmount === "" || tempAmount < 0) {
+    errorMessage.classList.remove("hide");
+  } else {
+    errorMessage.classList.add("hide");
+    amount.innerHTML = tempAmount;
+  }
+  balanceValue.innerText = tempAmount - expenditureValue.innerText;
+  totalAmount.value = "";
+});
 
 //Function To Disable Edit and Delete Button
+const disableButtons = (bool) => {
+  let editButtons = document.getElementsByClassName("edit");
+  Array.from(editButtons).forEach((element) => {
+    element.disabled = bool;
+  });
+};
+
+//Function To Modify List Elements
+const modifyElement = (element, edit = false) => {
+  let parentDiv = element.parentElement;
+  let currentBalance = balanceValue.innerText;
+  let currentExpense = expenditureValue.innerText;
+  let parentAmount = parentDiv.querySelector(".amount").innerText;
+  if (edit) {
+    let parentText = parentDiv.querySelector(".product").innerText;
+    productTitle.value = parentText;
+    userAmount.value = parentAmount;
+    disableButtons(true);
+  }
+  balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
+  expenditureValue.innerText =
+    parseInt(currentExpense) - parseInt(parentAmount);
+  parentDiv.remove();
+};
